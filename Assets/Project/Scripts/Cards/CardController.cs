b31c0678dc1c944571998bb6b5edddab175f7e9b
Sebasgmv <70557877+Sebasgmv@users.noreply.github.com>
2024-03-17 22:19:40 -0500

    Cards
    Archivos necesarios para manejo de cartas

---
+++ Assets/Project/Scripts/Cards/CardController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+// public enum CardType {
+//     Pilot,
+//     Weapon,
+//     CampEffect
+// }
+
+public interface ICardController {
+    void InitCard(string cardName, string cardDescription, int scrapCost, int scrapRecovery, bool isCampEffect,
+        Sprite imageSource);
+
+    void ShowCard();
+    // CardType GetCardType();
+}
+
+public class CardController : ICardController {
+    private readonly ICardView _view;
+
+    private string _cardName;
+    private string _cardDescription;
+    private int _scrapCost;
+    private int _scrapRecovery;
+    private bool _isCampEffect;
+    private Sprite _imageSource;
+
+    public CardController(ICardView view) {
+        _view = view;
+    }
+
+    public void InitCard(string cardName, string cardDescription, int scrapCost, int scrapRecovery, bool isCampEffect,
+        Sprite imageSource) {
+        _cardName = cardName;
+        _cardDescription = cardDescription;
+        _scrapCost = scrapCost;
+        _scrapRecovery = scrapRecovery;
+        _isCampEffect = isCampEffect;
+        _imageSource = imageSource;
+
+        SetCardUI();
+    }
+
+    protected virtual void SetCardUI() {
+        _view.SetCardUI(_cardName, _cardDescription, _scrapCost, _imageSource);
+    }
+
+    public virtual void ShowCard() {
+        Debug.Log($"opening the card");
+    }
+
+    // public abstract CardType GetCardType();
+}
---
+++ Assets/Project/Scripts/Cards/CardView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface ICardView {
+    void SetCardUI(string cardName, string cardDescription, int scrapCost, Sprite imageSource);
+}
+
+[Serializable]
+public class CardView : MonoBehaviour, ICardView {
+    [BoxGroup("Card UI Components")]
+    [SerializeField] private TMP_Text nameTMP;
+    [SerializeField] private TMP_Text descriptionTMP;
+    [SerializeField] private TMP_Text scrapCostTMP;
+    [SerializeField] private Image imageSourceIMG;
+    
+    private ICardController _cardController;
+
+    public ICardController CardController {
+        get { return _cardController ??= new CardController(this); }
+    }
+    
+    public virtual void SetCardUI(string cardName, string cardDescription, int scrapCost, Sprite imageSource) {
+        nameTMP.text = cardName;
+        descriptionTMP.text = cardDescription;
+        scrapCostTMP.text = $"{scrapCost}";
+        imageSourceIMG.sprite = imageSource;
+    }
+}
+
+public class Test {
+    public void TestMethod() {
+        var cardView = new CardView();
+        cardView.CardController.InitCard("Card Name", "Card Description", 10, 10, false, null);
+    }    
+}
