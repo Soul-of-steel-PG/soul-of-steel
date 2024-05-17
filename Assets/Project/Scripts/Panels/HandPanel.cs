c84121603165a9fe9aa160babbd28b7bdc45dc85
EDWIN VESGA <vesga.e@javeriana.edu.co>
2024-05-16 22:05:49 -0500

    Sincronización total del proyecto para paso a QA, ajustes en paneles de juego y otros scripts.

--- Assets/Project/Scripts/Panels/HandPanel.cs
+++ Assets/Project/Scripts/Panels/HandPanel.cs
@@ -2,12 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandPanel : MonoBehaviour {
+public interface IHandPanel
+{
+    void ResetAnimationReferenceParent();
+    GameObject GetGo();
+    Transform GetAnimationReference();
+}
+
+public class HandPanel : MonoBehaviour, IHandPanel
+{
     [SerializeField] private bool isMiddle;
     public Transform animationReference;
 
-    void Start() {
-        if (isMiddle) GameManager.Instance.middlePanel = this;
-        else GameManager.Instance.handPanel = this;
+    void Start()
+    {
+        if (isMiddle) GameManager.Instance.MiddlePanel = this;
+        else GameManager.Instance.HandPanel = this;
+    }
+
+    public void ResetAnimationReferenceParent()
+    {
+        animationReference.SetParent(transform.parent);
+    }
+
+    public GameObject GetGo()
+    {
+        return gameObject;
+    }
+
+    public Transform GetAnimationReference()
+    {
+        return animationReference;
     }
 }
